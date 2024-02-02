8c9e71bb4ea915eb279875dddb40ec1cc762ff79
PassinThomas <83069759+PassinThomas@users.noreply.github.com>
2024-02-02 20:24:32 +0000

    Fri, Feb 2, 2024, 9:24 PM +01:00

--- srcs/check_map/path_finding.c
+++ srcs/check_map/path_finding.c
@@ -10,12 +10,12 @@ char **map_copy(t_data *data, char **map_copy)
     map_copy = (char **)malloc(sizeof(char *) * (len + 2));
     if (!map_copy)
         return (NULL);
-    while (map_copy[i])
+    while (map_copy && i < len)
     {
-            map_copy[i] = ft_strdup(data->map[i]);
-            if (!map_copy[i])
-                return (ft_free_map(map_copy), NULL);
-            i++;
+        map_copy[i] = ft_strdup(data->map[i]);
+        if (!map_copy[i])
+            return (ft_free_map(map_copy), NULL);
+        i++;
     }
     map_copy[i] = NULL;
     return (map_copy);
