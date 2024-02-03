564f6b7c7235339a5090f279ecd44f820b94a37c
PassinThomas <83069759+PassinThomas@users.noreply.github.com>
2024-02-03 18:06:56 +0000

    Sat, Feb 3, 2024, 7:06 PM +01:00

--- srcs/check_map/path_finding.c
+++ srcs/check_map/path_finding.c
@@ -52,6 +52,7 @@ void get_position(t_data *data)
 
 static void flood_fill(char **map, int x, int y, t_data *data)
 {
+    printf("%d\n%d\n", data->info.line, data->info.col);
     if (x < 0 || x >= data->info.line || y < 0 || y >= data->info.col)
         return ;
     if (map[x][y] == '1')
