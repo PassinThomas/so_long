6363def15c45fb5ccb906c63c74aaca2588fe3d7
PassinThomas <83069759+PassinThomas@users.noreply.github.com>
2024-02-03 18:20:38 +0000

    Sat, Feb 3, 2024, 7:20 PM +01:00

--- srcs/check_map/path_finding.c
+++ srcs/check_map/path_finding.c
@@ -50,7 +50,7 @@ void get_position(t_data *data)
     return ;
 }
 
-static void flood_fill(char **map, int x, int y, t_data *data)
+void flood_fill(char **map, int x, int y, t_data *data)
 {
     if (x < 0 || x >= data->info.line || y < 0 || y >= data->info.col)
         return ;
