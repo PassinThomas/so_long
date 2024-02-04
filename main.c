0e7f77cdfd7efde55e34785e957a306a9de7f06d
PassinThomas <83069759+PassinThomas@users.noreply.github.com>
2024-02-04 07:47:01 +0000

    Sun, Feb 4, 2024, 8:47 AM +01:00

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 18:56:42 by tpassin           #+#    #+#             */
-/*   Updated: 2024/02/04 08:41:46 by marvin           ###   ########.fr       */
+/*   Updated: 2024/02/04 08:46:18 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,27 @@ int main(int ac, char **av)
 	data = malloc(sizeof(t_data));
 	if (!data)
 		return (printf("faillure malloc\n"), 0);
+	map_cp = NULL;
 	if (init_map(data, av[1]))
 	{
 		map_cp = map_copy(data);
+		if (map_cp)
+		{
+			while (map_cp[i])
+			{
+				j = 0;
+				while (map_cp[i][j])
+				{
+					if (map_cp[i][j] == 'P')
+					{
+						data->pos.x = i;
+						data->pos.y = j;
+					}
+					j++;
+				}
+				i++;
+			}
+		}
 		check_win(map_cp, data);
 		ft_free_map(data->map);
 		close(data->fd);
