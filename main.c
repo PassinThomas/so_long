2c168de990e98b917770fd6638e9f6f992677377
PassinThomas <83069759+PassinThomas@users.noreply.github.com>
2024-02-04 08:08:04 +0000

    Sun, Feb 4, 2024, 9:08 AM +01:00

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 18:56:42 by tpassin           #+#    #+#             */
-/*   Updated: 2024/02/04 09:05:20 by marvin           ###   ########.fr       */
+/*   Updated: 2024/02/04 09:07:24 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,7 @@ int main(int ac, char **av)
 		check_win(map_cp, data);
 		ft_free_map(data->map);
 		close(data->fd);
-		free(data);
-		return (0);
 	}
-	close(data->fd);
 	free(data);
 	return (0);
 }
