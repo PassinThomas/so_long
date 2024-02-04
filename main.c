b9722b7d91f104095ae1e38972ff818d02cd8929
PassinThomas <83069759+PassinThomas@users.noreply.github.com>
2024-02-04 01:32:30 +0000

    Sun, Feb 4, 2024, 2:32 AM +01:00

---
+++ main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 18:56:42 by tpassin           #+#    #+#             */
+/*   Updated: 2024/02/04 02:31:03 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/so_long.h"
+
+int main(int ac, char **av)
+{
+	int i = 0;
+	int j;
+	char **map_cp;
+
+	t_data *data;
+	if (ac != 2)
+		return (printf("Error\n2args required\n"), 0);
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (printf("faillure malloc\n"), 0);
+	map_cp = NULL;
+	if (init_map(data, av[1]))
+	{
+		map_cp = map_copy(data);
+		check_win(map_cp, data);
+		ft_free_map(data->map);
+		close(data->fd);
+		free(data);
+		return (0);
+	}
+	close(data->fd);
+	free(data);
+	return (0);
+}
